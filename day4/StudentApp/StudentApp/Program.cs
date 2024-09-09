76ae8cda5b732f3ff10326749bb279eead8a63ed
Rohit Laishram <laishramrohit15@gmail.com>
2024-09-09 14:31:11 +0530

    student

---
+++ day4/StudentApp/StudentApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student(100, "rohit laishram", new DateTime(2003, 04, 15), 7642833064, "laishramrohit15@gmail.com");
+            std1.displayId();
+        }
+    }
+}
---
+++ day4/StudentApp/StudentApp/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Student
+    {
+        public int studentid { get; set; }
+        public string name { get; set; }
+        public DateTime dob { get   ; set; }
+        public long phoneNumber { get; set; }
+        public string email { get; set; }
+        public Student(int studentid,string name, DateTime dob, long phoneNumber,string email)
+        {
+            this.studentid = studentid;
+            this.name = name;
+            this.dob= dob;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public void displayId()
+        {
+            Console.WriteLine($"ID : {this.studentid}");
+            Console.WriteLine($"Name : {this.name}");
+            Console.WriteLine($"Date of Birth : {this.dob}");
+            Console.WriteLine($"Phone : {this.phoneNumber}");
+            Console.WriteLine($"email : {this.email}");
+
+        }
+    }
+}
