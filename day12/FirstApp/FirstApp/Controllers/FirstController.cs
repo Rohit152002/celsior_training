d89751e3ef121492c07961482a6bbc147119606f
Rohit Laishram <rohit.laishram@pyramidconsultinginc.com>
2024-09-19 19:33:52 +0530

    day12

---
+++ day12/FirstApp/FirstApp/Controllers/FirstController.cs
@@ -0,0 +1,23 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+       
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Customer = new Customer { Id = 1, Name = "Rohit", Age = 12 };
+            ViewData["Customer Name"] = "Rohit Laishram";
+            return View();
+        }
+
+        public IActionResult ViewCustomerData()
+        {
+            Customer customer = new Customer { Id = 1, Name = "John Doe", Age = 12 };
+            return View(customer);
+        }
+
+    }
+}
---
+++ day12/FirstApp/FirstApp/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+	public class ProductController : Controller
+	{
+		List<Product> products = new List<Product> { new Product { Id = 1, Name = "Book", Description = "Book1 Description", Image = "https://media.gettyimages.com/id/157482029/photo/stack-of-books.jpg?s=612x612&w=gi&k=20&c=_Yaofm8sZLZkKs1eMkv-zhk8K4k5u0g0fJuQrReWfdQ=", Price = 100, Quantity = 1 }, new Product { Id = 2, Name = "Book2", Description = "Book2 Description", Image = "https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?cs=srgb&dl=pexels-pixabay-159866.jpg&fm=jpg", Price = 200, Quantity = 3 } };
+
+		public IActionResult Index()
+		{
+			return View(products);
+		}
+	}
+}
